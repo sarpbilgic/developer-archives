fa0767ad3556d6003aec9bced8b04f80cd0e6f55
Sarp Bilgiç <124276905+ashesofthewake0@users.noreply.github.com>
2025-12-15 21:12:09 +0300

    settings file fix after migrating neon

--- app/settings.py
+++ app/settings.py
@@ -8,21 +8,25 @@ logger = logging.getLogger(__name__)
 
 class Settings:
     def __init__(self):
+        self._database_url = os.getenv("DATABASE_URL")
+        """
         self.db_host = os.getenv("DB_HOST")
         self.db_port = os.getenv("DB_PORT")
         self.db_name = os.getenv("DB_NAME")
         self.db_username = os.getenv("DB_USERNAME")
         self.db_password = os.getenv("DB_PASSWORD")
+        """
         self.aws_region = os.getenv("AWS_REGION")
         self.github_api_token = os.getenv("GITHUB_API_TOKEN")
         self.sqs_queue_url = os.getenv("SQS_URL")
         self.dynamodb_table_name = os.getenv("DYNAMODB_TABLE_NAME")
         self.s3_readme_bucket = os.getenv("S3_README_BUCKET")  
     
-        if self.db_host is None:
+        if self._database_url is None:
             logger.info("Local .env file not found, loading settings from AWS Parameter Store")
             self._load_from_ssm()
 
+    
     def _load_from_ssm(self):
         ssm_client = boto3.client('ssm', region_name=os.getenv("AWS_REGION", "eu-central-1"))
         def get_parameter(name, with_decryption=False):
@@ -37,27 +41,23 @@ class Settings:
                 logger.warning(f"SSM parameter not found: {name}")
                 return None
 
-        self.db_host = get_parameter('/dev/developer-archives/DB_ENDPOINT')
-        self.db_port = get_parameter('/dev/developer-archives/DB_PORT')
-        self.db_name = get_parameter('/dev/developer-archives/DB_NAME')
-        self.db_username = get_parameter('/dev/developer-archives/DB_USERNAME')
+        self._database_url = get_parameter('/dev/developer-archives/DATABASE_URL')
         self.aws_region = get_parameter('/dev/developer-archives/AWS_REGION')
         self.sqs_queue_url = get_parameter('/dev/developer-archives/SQS_URL')
         self.dynamodb_table_name = get_parameter('/dev/developer-archives/DYNAMODB_TABLE_NAME')
         self.s3_readme_bucket = get_parameter('/dev/developer-archives/S3_README_BUCKET')  
-
-        self.db_password = get_parameter('/dev/developer-archives/DB_PASSWORD', with_decryption=True)
         self.github_api_token = get_parameter('/dev/developer-archives/GITHUB_API_TOKEN', with_decryption=True)
         
 
-    @property
+    @cached_property
     def database_url(self) -> str:
-        """Dynamically builds the database URL after settings are loaded."""
-        if not all([self.db_username, self.db_password, self.db_host, self.db_port, self.db_name]):
-             raise ValueError("Database connection details are missing.")
-        return (
-            f"postgresql+asyncpg://{self.db_username}:{self.db_password}"
-            f"@{self.db_host}:{self.db_port}/{self.db_name}"
-        )
+        url = self._database_url
+        if not url:
+            raise ValueError("Database connection details are missing.")
+            
+        if url.startswith("postgresql://"):
+            url = url.replace("postgresql://", "postgresql+asyncpg://", 1)
+            
+        return url
 
 settings = Settings()
